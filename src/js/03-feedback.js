714e0bd541fb858a1e121fa16b25a9500a4abda5
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-04-29 22:14:57 +0200

    ad 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import { throttle } from 'lodash';
 
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+const saveInputs = throttle(() => {
+  const values = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(values));
+}, 500);
+
+emailInput.addEventListener('input', saveInputs);
+messageInput.addEventListener('input', saveInputs);
+
+window.addEventListener('load', () => {
+  const getValues = localStorage.getItem('feedback-form-state');
+  if (getValues > '') {
+    const values = JSON.parse(getValues);
+    emailInput.value = values.email;
+    messageInput.value = values.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const values = localStorage.getItem('feedback-form-state');
+  console.log(values);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
